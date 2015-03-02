91a4b8191cd9f3af3ad4b650335d04c6afe803bf
Cambrian Liu <Cambrian.Liu@gmail.com>
2015-03-02 14:33:10 -0800

    update

---
+++ dmi/Batch_stitch.py
@@ -0,0 +1,15 @@
+import glob
+import os
+
+dc = DirectoryChooser("Choose directory of LSM files...")
+dirPath = dc.getDirectory()
+fileList = glob.glob(dirPath+"*.lsm")
+
+for path in fileList:
+	basename = os.path.splitext(os.path.basename(path))[0]
+	outputPath = dirPath + basename + "_stitched"
+	if not os.path.exists(outputPath):
+		os.mkdir(outputPath)
+
+	params = "type=[Positions from file] order=[Defined by image metadata] browse=[" + path + "] multi_series_file=[" + path + "] fusion_method=[Linear Blending] regression_threshold=0.30 max/avg_displacement_threshold=2.50 absolute_displacement_threshold=3.50 compute_overlap ignore_z_stage increase_overlap=10 subpixel_accuracy computation_parameters=[Save memory (but be slower)] image_output=[Write to disk] output_directory=[" + outputPath + "]";
+	IJ.run("Grid/Collection stitching", params)
